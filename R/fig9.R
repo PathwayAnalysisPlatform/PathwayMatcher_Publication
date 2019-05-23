43228ccf46e89197a51398b22ddbcd39553b316f
LuisFranciscoHS <Luis.Sanchez@uib.no>
2019-05-23 13:45:04 +0200

    Renamed figure files

--- R/fig9.R
+++ R/fig9.R
@@ -1,6 +1,7 @@
 # 
-# Plot two individual proteoform matching percentages. 
-# Each proteoform is alterd and matched against proteoforms in the database that share the same accession.
+# Plot percentage of proteoforms matched with the phosphoproteoforms created from an experimental dataset.
+# (Ochoa D, Jarnuczak AF, Gehre M, Soucheray M, Kleefeldt AA, Vieitez C, et al. The functional landscape of the human phosphoproteome. bioRxiv. 2019:541656. doi:10.1101/541656, Supp table 2)
+# Each proteoform is matched against proteoforms in the database that share the same accession.
 # From all possible proteoforms, we calculate the percentage of them that got matched using each matching.
 #
 start_time <- Sys.time()
@@ -13,38 +14,20 @@ library(plyr)
 source("R/plotPercentages.R")
 
 # Parameters
-percentagesFile1 <- "resources/sensitivity/matchesFile1.csv"
-percentagesFile2 <- "resources/sensitivity/matchesFile2.csv"
+percentagesFile <- "resources/sensitivity/percentagesFilePhosphoproteoforms.csv"
 
 # Main script
 
-print(paste("Loading data from", percentagesFile1))
-percentages1 <- read.csv(percentagesFile1, header = T)
-print(paste("Loading data from", percentagesFile2))
-percentages2 <- read.csv(percentagesFile2, header = T)
-
+print(paste("Loading data from", percentagesFile))
+percentages <- read.csv(percentagesFile, header = T)
 
 print(paste("Plotting percentages"))
-plot1 <- plotPercentagesSeparated(percentages1)
-plot1
-plot2 <- plotPercentagesSeparated(percentages2)
-plot2
+percentagesPlot <- plotPercentages(percentages)
+percentagesPlot
 
-print("Making grid")
-grid <- plot_grid(plot1 + theme(legend.position = "none"), 
-                  plot2 + theme(legend.position = "none"),
-                  labels = c("A", "B"),
-                  align = "vh",
-                  hjust = -1, nrow = 1)
-legend <- get_legend(plot1)
-grid <- plot_grid(grid, legend, rel_widths = c(2, 0.3))
-grid
-print("Saving plot")
-save_plot("docs/fig9.png", grid,
-          ncol = 2, nrow =1,
-          base_aspect_ratio = 2:1
-)
+png("docs/fig11.png", height = 12, width = 12, units = "cm", res = 600)
+plot(percentagesPlot)
+dummy <- dev.off()
 
 print("Finished")
 print(Sys.time() - start_time)
-
